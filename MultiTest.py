2d03857a949231c08a667de1e8d48e8f854ee501
jsllby <857700847@qq.com>
2018-09-18 20:54:39 +0800

    测试时间

--- MultiTest.py
+++ MultiTest.py
@@ -37,36 +37,38 @@ class myThread(threading.Thread):
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-TEST_NAME = 'malware-test-v0'
-test_env = locals()
-test_thread = locals()
-start_time = datetime.datetime.now()
-for i in range(100):
-    test_env['env' + str(i)] = gym.make(TEST_NAME)
-    test_thread['thread' + str(i)] = myThread(test_env.get('env' + str(i)), i)
-    test_thread.get('thread' + str(i)).start()
-for i in range(100):
-    test_thread.get('thread' + str(i)).join()
-end_time = datetime.datetime.now()
-print("total time is %s " % (end_time - start_time))
-
 # TEST_NAME = 'malware-test-v0'
-# env_test = gym.make(TEST_NAME)
+# test_env = locals()
+# test_thread = locals()
 # start_time = datetime.datetime.now()
-# for i in range(10):
-#     R = 0
-#     test_state = env_test.reset()
-#     for step in range(100):
-#         action = env_test.action_space.sample()
-#         observation, reward, done, info = env_test.step(action)
-#         # logger.info("thread %s: action %s  observation %s" % (self.threadID, action, observation))
-#         R += reward
-#         if done:
-#             logger.info("thread %s: step = %s  reward = %s" % (i, step, R))
-#             # print("thread %s: step = %s  reward = %s" % (self.threadID, step, R))
-#             break
+# for x in range (10):
+#     for i in range(10):
+#         test_env['env' + str(i)] = gym.make(TEST_NAME)
+#         test_thread['thread' + str(i)] = myThread(test_env.get('env' + str(i)), i)
+#         test_thread.get('thread' + str(i)).start()
+#     for i in range(10):
+#         test_thread.get('thread' + str(i)).join()
+#     logger.info("%s  ends"%x)
 # end_time = datetime.datetime.now()
 # print("total time is %s " % (end_time - start_time))
+
+TEST_NAME = 'malware-test-v0'
+env_test = gym.make(TEST_NAME)
+start_time = datetime.datetime.now()
+for i in range(100):
+    R = 0
+    test_state = env_test.reset()
+    for step in range(100):
+        action = env_test.action_space.sample()
+        observation, reward, done, info = env_test.step(action)
+        # logger.info("thread %s: action %s  observation %s" % (self.threadID, action, observation))
+        R += reward
+        if done:
+            logger.info("thread %s: step = %s  reward = %s" % (i, step, R))
+            # print("thread %s: step = %s  reward = %s" % (self.threadID, step, R))
+            break
+end_time = datetime.datetime.now()
+print("total time is %s " % (end_time - start_time))
 
 # # 创建新线程
 # thread1 = myThread(1, "Thread-1", 1)
