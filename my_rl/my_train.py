280c96e55768badcd2836acb90c2368f388ecded
jsllby <857700847@qq.com>
2018-09-27 13:40:39 +0800

    修改reward

--- my_rl/my_train.py
+++ my_rl/my_train.py
@@ -56,6 +56,10 @@ def train_agent(agent, env, steps, outdir, max_episode_len=None,
             # o_{t+1}, r_{t+1}
             obs, r, done, info = env.step(action)
             t += 1
+            if r == 0:
+                r = -1
+            else:
+                r = 10
             episode_r += r
             episode_len += 1
 
