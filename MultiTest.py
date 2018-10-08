b6dddfebbabc651143b69c236680eb283c618c6a
jsllby <857700847@qq.com>
2018-10-08 14:44:37 +0800

    测试reset with id

--- MultiTest.py
+++ MultiTest.py
@@ -10,6 +10,7 @@ from multiprocessing import Pool, Process, Value, Lock
 import multiprocessing
 import os
 import argparse
+import numpy
 
 lock = Lock()
 counter = Value("i", 0)
@@ -22,7 +23,7 @@ logger.setLevel(logging.INFO)
 path = "test_log.txt"
 
 
-def test(id):
+def test(id, scores):
     global lock, counter
     with open(path, 'a+') as f:
         f.write("run process {} ....\n".format(id))
@@ -42,7 +43,12 @@ def test(id):
                     counter.value += step
                 end = datetime.datetime.now()
                 process_time = end - start
-                logger.info("Process {} runs {} with {} steps.\n".format(id, process_time, step))
+                if R > 0:
+                    R = 10
+                scores[id] = float(R)
+                logger.info(
+                    "Process {} runs {} with {} steps  reward = {}   socres = {} .\n".format(id, process_time, step, R,
+                                                                                             scores[id]))
                 with open(path, 'a+') as f:
                     f.write("Process {} runs {} with {} steps.\n".format(id, process_time, step))
                 break
@@ -53,14 +59,16 @@ def test(id):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--number', type=int, default=3)
+    parser.add_argument('--number', type=int, default=100)
     args = parser.parse_args()
+
+    scores = multiprocessing.Array('f', numpy.zeros(args.number))
 
     with open(path, 'a+') as f:
         f.write("start time is {} \n".format(datetime.datetime.now()))
 
     for i in range(args.number):
-        test_process['Process' + str(i)] = Process(target=test, args=(i,))
+        test_process['Process' + str(i)] = Process(target=test, args=(i, scores))
         test_process.get('Process' + str(i)).start()
 
     print('Wait all processed end.\n')
