470275ad6ae289579ec009575acf887455129139
jsllby <857700847@qq.com>
2018-10-11 09:59:38 +0800

    debug

--- train.py
+++ train.py
@@ -51,7 +51,7 @@ def main():
     parser.add_argument('--soft-update-tau', type=float, default=1e-2)
     parser.add_argument('--update-interval', type=int, default=1)
     parser.add_argument('--eval-n-runs', type=int, default=100)
-    parser.add_argument('--eval-interval', type=int, default=100)
+    parser.add_argument('--eval-interval', type=int, default=1000)
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--minibatch-size', type=int, default=None)
     parser.add_argument('--test-random', action='store_true')
